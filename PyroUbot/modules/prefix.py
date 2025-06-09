475819a85a709328165baf27b4b5e6570c0101a9
S1teg4a <rafaszmotenzy@gmail.com>
2025-06-09 22:24:11 +0700

    Update prefix.py

--- PyroUbot/modules/prefix.py
+++ PyroUbot/modules/prefix.py
@@ -3,5 +3,5 @@ __HELP__ = """
 <blockquote><b>Bantuan untuk prefix
 
 perintah : <code>{0}prefix</code> [simbol]
-    untuk merubah prefix/handler perintah, tutor none prefix atau tanpa prefix: <a href=https://t.me/sitegaame/18>KLIK DISINI</a> </b></blockquote>
+    untuk merubah prefix/handler perintah, tutor none prefix atau tanpa prefix: <a href=https://t.me/marketplacemee/2>KLIK DISINI</a> </b></blockquote>
 """
