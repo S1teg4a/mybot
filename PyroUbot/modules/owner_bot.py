5bbb970055a7e349f647b37f2e64d91523f1a5c8
S1teg4a <rafaszmotenzy@gmail.com>
2025-06-20 16:00:40 +0700

    Update owner_bot.py

--- PyroUbot/modules/owner_bot.py
+++ PyroUbot/modules/owner_bot.py
@@ -12,7 +12,7 @@ async def _(client, message):
     pt_id = await get_list_from_vars(client.me.id, "PT_USERS")
     admin_id = await get_list_from_vars(client.me.id, "ADMIN_USERS")
     seller_id = await get_list_from_vars(client.me.id, "SELER_USERS")
-    if user.id not in pt_id and user.id not in admin_id and user.id not in seller_id and != OWNER_ID:
+    if user.id not in pt_id and user.id not in admin_id and user.id not in seller_id and user.id != OWNER_ID:
         return
     user_id, get_bulan = await extract_user_and_reason(message)
     msg = await message.reply("memproses...")
