ad610b8aa4b0f3eb71d65e535ae559fab01ec053
S1teg4a <rafaszmotenzy@gmail.com>
2025-06-20 15:44:10 +0700

    Update _cmd.py

--- PyroUbot/core/helpers/_cmd.py
+++ PyroUbot/core/helpers/_cmd.py
@@ -40,7 +40,7 @@ class PY:
         async def function(client, message):
             user = message.from_user
             pt_id = await get_list_from_vars(client.me.id, "PT_USERS")
-            if user.id not in pt_id and uid != OWNER_ID:
+            if user.id not in pt_id and != OWNER_ID:
                 return
             return await func(client, message)
 
@@ -53,7 +53,7 @@ class PY:
             pt_id = await get_list_from_vars(client.me.id, "PT_USERS")
             admin_id = await get_list_from_vars(client.me.id, "ADMIN_USERS")
             seller_id = await get_list_from_vars(client.me.id, "SELER_USERS")
-            if user.id not in pt_id and user.id not in admin_id and user.id not in seller_id and uid != OWNER_ID:
+            if user.id not in pt_id and user.id not in admin_id and user.id not in seller_id and != OWNER_ID:
                 return
             return await func(client, message)
 
@@ -65,7 +65,7 @@ class PY:
             user = message.from_user
             pt_id = await get_list_from_vars(client.me.id, "PT_USERS")
             admin_id = await get_list_from_vars(client.me.id, "ADMIN_USERS")
-            if user.id not in pt_id and user.id not in admin_id and uid != OWNER_ID:
+            if user.id not in pt_id and user.id not in admin_id and != OWNER_ID:
                 return
             return await func(client, message)
 
